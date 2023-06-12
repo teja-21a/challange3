2c6bc2419380348b8e4b002f5968ebe12fde4bfa
tej <pavanteja123ll@gmail.com>
2023-06-12 14:53:12 +0530

    duck commit

---
+++ duck.py
@@ -0,0 +1,9 @@
+class Duck:
+    def eat(self):
+        print("I eat worms ")
+
+    def sleep(self):
+        print("I can Sleep in water")
+
+    def bark(self):
+        print("I can't bark    ")
